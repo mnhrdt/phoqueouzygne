c2567a62fb493bed7ab6b4797bd326b6a90c449d
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2018-11-03 19:09:41 +0100

    [doc] text and experiments with discrete chirps

--- doc/a/ch.sh
+++ doc/a/ch.sh
@@ -1,29 +1,37 @@
 #!/bin/sh
 
 cat <<END | octave
-K = 0.6;
+K = 2;
 T = 5;
-N = 101;
+N = 1001;
 x = linspace(-3*T, 3*T, N);
 dx = (x(2)-x(1));
 h = exp(i*K*x.*x) .* (abs(x)<T);
 f = conv(h, conj(h), "same") * dx;
-s = (abs(x)<2*T) .* sinc(K*x.*(2*T-abs(x))/pi);
 s = (abs(x)<2*T) .* (2*T-abs(x)) .* sinc(K*x.*(2*T-abs(x))/pi);
-dlmwrite("o/x.txt", x, "\n");
-dlmwrite("o/hr.txt", real(h), "\n");
-dlmwrite("o/hi.txt", imag(h), "\n");
-dlmwrite("o/fr.txt", real(f), "\n");
-dlmwrite("o/fi.txt", imag(f), "\n");
-dlmwrite("o/s.txt", s, "\n");
+xhfs = [x ; real(h) ; imag(h) ; real(f) ; imag(f) ; s ];
+dlmwrite("o/xhfs", xhfs', " ");
 END
-
-paste o/x.txt o/hr.txt > o/hr
-paste o/x.txt o/hi.txt > o/hi
-paste o/x.txt o/fr.txt > o/fr
-paste o/x.txt o/fi.txt > o/fi
-paste o/x.txt o/s.txt > o/s
 
 cat <<END | gnuplot -persist
-plot [:] [-3:12] "o/hr" w lines, "o/hi" w lines, "o/fr" w lines, "o/fi" w lines, "o/s" w points pt 1
+set zeroaxis
+set title "N=101"
+sinc(x)=sin(x)/x
+K=2
+T=5
+plot [-12:12] [-2.5:11] \
+	"o/xhfs" using 1:4 w points title "Re(h*h)" lt 2, \
+	((abs(x)<2*T)*(2*T-abs(x))*sinc(K*x*(2*T-abs(x))))**1 w lines title "s" lt 1
 END
+
+#$paste o/x.txt o/hr.txt > o/hr
+#$paste o/x.txt o/hi.txt > o/hi
+#$paste o/x.txt o/fr.txt > o/fr
+#$paste o/x.txt o/fi.txt > o/fi
+#$paste o/x.txt o/s.txt > o/s
+#
+#cat <<END | gnuplot -persist
+#plot [:] [-3:12] "o/hr" w lines, "o/hi" w lines, "o/fr" w lines, "o/fi" w lines, "o/s" w points pt 1
+#END
+
+#head o/xhfs
